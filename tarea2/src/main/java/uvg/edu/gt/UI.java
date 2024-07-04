0d9f368f8c3ef3bbc6f3abe666d0993c37fdf804
Rodrigo Ramirez <131787074+Guallitoprogra@users.noreply.github.com>
2024-07-04 12:00:09 -0600

    2 Update UI

--- tarea2/src/main/java/uvg/edu/gt/UI.java
+++ tarea2/src/main/java/uvg/edu/gt/UI.java
@@ -8,9 +8,15 @@ public class UI {
         Scanner scanner = new Scanner(System.in);
         Calc calculadora = new Calc();
 
-        System.out.println("Ingrese el primer número");
+        System.out.println("Ingrese el primer número:");
         int n1 =scanner.nextInt();
+        
+        System.out.println("Ingrese el segundo número:");
+        int n2 = scanner.nextInt();
 
+
+        System.out.println("Elija una operación: 1. Suma 2. Resta 3. Multiplicacion");
+        int opcion = scanner.nextInt();
 
 
 }
