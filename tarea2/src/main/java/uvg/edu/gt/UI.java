83c4002c28a1668872301a5985842c58aac1b75d
Rodrigo Ramirez <131787074+Guallitoprogra@users.noreply.github.com>
2024-07-04 11:55:20 -0600

    Update of class UI

--- tarea2/src/main/java/uvg/edu/gt/UI.java
+++ tarea2/src/main/java/uvg/edu/gt/UI.java
@@ -1,5 +1,17 @@
 package uvg.edu.gt;
 
+import java.util.Scanner;
+
 public class UI {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calc calculadora = new Calc();
+
+        System.out.println("Ingrese el primer número");
+        int n1 =scanner.nextInt();
+
+
+
+}
 }
