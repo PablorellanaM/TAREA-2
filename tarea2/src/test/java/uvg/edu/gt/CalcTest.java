baf1ad623c4ee6a53bb042d59bc545707b6b4b8d
dils1809 <134822987+dils1809@users.noreply.github.com>
2024-07-04 12:07:51 -0600

    TestSuma

--- tarea2/src/test/java/uvg/edu/gt/CalcTest.java
+++ tarea2/src/test/java/uvg/edu/gt/CalcTest.java
@@ -1,6 +1,14 @@
 package uvg.edu.gt;
 
 //Testing unitario
-public class CalcTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class CalcTest {
+    @Test
+    public void testSuma() {
+        Calc calculadora = new Calc();
+        assertEquals(5, calculadora.suma(2, 3));
+    }
 }
