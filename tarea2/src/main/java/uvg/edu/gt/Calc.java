c3238cf31f5a1d55b477275e6596843735786035
PablorellanaM <ore20555@uvg.edu.gt>
2024-07-04 11:47:30 -0600

    Crear Metodo de Suma

--- tarea2/src/main/java/uvg/edu/gt/Calc.java
+++ tarea2/src/main/java/uvg/edu/gt/Calc.java
@@ -3,4 +3,8 @@ package uvg.edu.gt;
 //Ejemplo cambio
 public class Calc {
 
+    public int suma(int n1, int n2) {
+        return n1 + n2;
+    }
+
 }
--- tarea2/src/test/java/uvg/edu/gt/CalcTest.java
+++ tarea2/src/test/java/uvg/edu/gt/CalcTest.java
@@ -3,8 +3,4 @@ package uvg.edu.gt;
 //Testing unitario
 public class CalcTest {
 
-    public int suma(int n1, int n2) {
-        return n1 + n2;
-    }
-
 }
